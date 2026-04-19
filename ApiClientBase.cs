e65baece6e4f82d071dd84dc7a6630a55ccf68cd
julia66677 <a.a.ch8535@gmail.com>
2026-04-19 20:14:03 +0300

    Update Client 1: add ApiClientBase and AuthClient

---
+++ ApiClientBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http.Json;
+
+namespace UwiTests.Client;
+
+public abstract class ApiClientBase
+{
+	protected readonly HttpClient _http;
+
+    protected ApiClientBase(HttpClient http)
+	{
+		_http = http;
+	}
+
+	public void SetAuthToken(string token)
+	{
+		_http.DefaultRequestHeaders.Authorization = 
+			new AuthorizationHeaderValue ("Bearer", token);
+
+    }
+}
---
+++ AuthClient.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Http.Json;
+
+namespace UwiTests.Client;
+
+public class AuthClient : ApiClientBase
+{
+	public AuthClient(HttpClient http) : base(http) { }
+
+	//регистрация
+	public async Task<string?> RegisterAsync(string userName, string email, string password)
+	{
+		var response = await _http.PostAsJsonAsync("api/auth/register", new 
+		{ 
+			userName, 
+			email, 
+			password 
+		});
+		response.EnsureSuccessStatusCode();
+		var result = await response.Content.ReadFromJsonAsync<TokenResponse>
+			();
+			return result?.Token;
+	}
+
+	//вход
+	public async Task<string?> LoginAsunc(string email, string password)
+	{
+		var rasponnse = await _http.PostAsJsongAsync("api/auth/login", new
+		{
+			email,
+			password
+		});
+		rasponnse.EnsureSuccessStatusCode();
+		var result = await response.Content.ReadFromJsonAsyn<TokenResponse>
+			();
+		return result?.Token;
+	}
+
+	internal class TokenResponse
+	{
+		public string Token { get; set; } = string.Empty;
+	}
+}
