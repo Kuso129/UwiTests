986d9a7caaeb00441bc127745887dd371b8e3b05
Chef1ri4 <bomzharizmaticnyj@gmail.com>
2026-05-04 20:00:41 +0300

    User data updated

--- UwiTests/Models/User.cs
+++ UwiTests/Models/User.cs
@@ -2,9 +2,10 @@
 {
     public class User
     {
-        public int Id { get; set; }
-        public string Login {  get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Id { get; set; } = string.Empty;
+        public string Login {  get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = "User"; // can be "Developer"
     }
 }
--- UwiTests/Models/UserEnter.cs
+++ UwiTests/Models/UserEnter.cs
@@ -2,8 +2,9 @@
 {
     public class UserEnter
     {
-        public string Login {  get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public string Login {  get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = "User"; // can be "Developer"
     }
 }
