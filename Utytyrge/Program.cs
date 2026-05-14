90c49e61fe6880ce5a7f8060ecb0807d8925f780
Comeng <kodzarenko@mail.ru>
2026-05-14 19:50:36 +0300

    test up setap

---
+++ Utytyrge/Program.cs
@@ -0,0 +1,25 @@
+﻿
+static class Programm
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Please enter URL:");
+        string url = Console.ReadLine();
+
+        Console.WriteLine("Please enter test case: 1 2 3 4");
+        string method = Console.ReadLine();
+    }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+ }
